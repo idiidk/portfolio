5511fde3790829fe8f1ac92fe9e4bc2f98e25975
Vincent Brouwer <dviide@protonmail.com>
2022-05-07 15:33:17 +0200

    madding import madness

--- plugins/pixi.client.js
+++ plugins/pixi.client.js
@@ -1,7 +1,12 @@
+import { Application, Graphics, filters } from 'pixi.js'
 import { KawaseBlurFilter } from '@pixi/filter-kawase-blur'
-import * as PIXI from 'pixi.js'
 
 export default (_, inject) => {
-  PIXI.filters.KawaseBlurFilter = KawaseBlurFilter
-  inject('PIXI', PIXI)
+  filters.KawaseBlurFilter = KawaseBlurFilter
+
+  inject('PIXI', {
+    Application,
+    Graphics,
+    filters,
+  })
 }
